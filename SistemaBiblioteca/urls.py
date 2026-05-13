6f968f817b97a004f7a2a96ef89e20d1b24e5de8
Diego Alfaro <diegoalfaropinto12@gmail.com>
2026-05-13 11:40:27 -0600

    FEAT: agregar endpoints para el manejo de usuarios

--- SistemaBiblioteca/urls.py
+++ SistemaBiblioteca/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     #API endpoints
     path('api/', include('autores.urls')),
     path('api/', include('libros.urls')),
+    path('api/', include('usuarios.urls')),
 
     # Documentacion
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
---
+++ usuarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import UsuarioViewSet
+
+router = DefaultRouter()
+router.register('usuarios', UsuarioViewSet, basename='usuarios')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
